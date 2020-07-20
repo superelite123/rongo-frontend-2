43ca2e57488c69e141f190eb5c655c66854ea1f2
Mobile Star <karel920@hotmail.com>
2020-07-20 19:30:49 +0800

    - update url

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -26,4 +26,4 @@ export const SHOW_LIVEPLAYER=221
 export const SHOW_LIVESTANDBYPANEL=231
 export const SHOW_LIVEPRODUCTLIST=232
 export const SHOW_LIVECHATPANEL=233
-export const BASE_URL = 'http://192.168.0.100:8000' // 'https://app.live.rongoinc.com'
+export const BASE_URL = 'https://app.live.rongoinc.com' // 'http://192.168.0.100:8000'
